1ac0d76b0c65bd89428c405483e1e9023c9dd63f
Devansh Sharma <sharmadevansh007777@gmail.com>
2025-01-26 16:14:03 +0000

    12/12 tasks done!

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
 console.log("Hello World");
@@ -9,6 +10,8 @@ stylePath = __dirname + '/public';
 
 
 app.use("/public", express.static(stylePath));
+//Use body-parser to Parse POST Requests
+app.use("/", bodyParser.urlencoded({extended: false}));
 
 // app.get("/", function(req, res) {
 //     res.send("Hello Express");
@@ -17,6 +20,16 @@ app.use("/public", express.static(stylePath));
 app.get("/", function(req,res){
     res.sendFile(absolutePath);
 });
+
+
+
+// app.use(<mware-function>)
+app.use("/", function(req, res, next){
+    console.log(req.method + " " + req.path + " - " + req.ip );
+    next();
+});
+
+
 
 app.get("/json", function(req,res){
 
@@ -27,7 +40,25 @@ app.get("/json", function(req,res){
         res.json({"message": "Hello json"});
     }
 });
+// Chain Middleware to Create a Time Server
+app.get("/now", function(req,res,next){
+    req.time = new Date().toString();
+    next();
+}, function(req,res){
+    res.json({"time": req.time})
+});
 
+// Get Route Parameter Input from the Client
+app.get("/:word/echo", function(req,res){
+    res.json({"echo": req.params.word});
+});
+
+//Get Query Parameter Input from the Client
+app.route("/name").get( function(req,res){
+    res.json({"name": req.query.first + " " + req.query.last});
+}).post(function(req,res){
+    res.json({"name": req.body.first + " " + req.body.last});
+});
 
 
 
