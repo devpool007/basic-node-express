7425c3a8eb1fe4560b09792306195fa115305a9d
Devansh Sharma <sharmadevansh007777@gmail.com>
2025-01-26 12:41:51 +0000

    6/12 tasks done

--- myApp.js
+++ myApp.js
@@ -1,32 +1,32 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
 
+absolutePath = __dirname + '/views/index.html';
+stylePath = __dirname + '/public';
 
 
+app.use("/public", express.static(stylePath));
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// })
 
+app.get("/", function(req,res){
+    res.sendFile(absolutePath);
+});
 
+app.get("/json", function(req,res){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message": "HELLO JSON"});
+    } 
+    else {
+        res.json({"message": "Hello json"});
+    }
+});
 
 
 
